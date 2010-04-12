a16a74eeb80606190407e6a60b02d9d26313cde4
Michael Ridgway <kokeeno@kokeeno.com>
2010-04-11 23:12:05 -0400

    Ready to go

---
+++ application/Blog/Controller/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Blog\Controller;
+
+class Index extends \Zend_Controller_Action
+{
+    public function index()
+    {
+        die('Success: You are in the blog controller.');
+    }
+}
